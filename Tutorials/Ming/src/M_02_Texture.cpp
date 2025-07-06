6a8c2a512abb2953e4f8165b6b3b9bc8887b8e17
automask <laputa_in@foxmail.com>
2025-07-06 16:04:21 +0800

    [auto] update

--- Tutorials/Ming/src/M_02_Texture.cpp
+++ Tutorials/Ming/src/M_02_Texture.cpp
@@ -14,12 +14,19 @@ namespace Diligent
 class Tutorial03_Texturing final : public SampleBase
 {
 public:
-    virtual void Initialize(const SampleInitInfo& InitInfo) override final;
+    virtual void                       Initialize(const SampleInitInfo& InitInfo) override final;
+    virtual void                       Render() override final;
+    virtual void                       Update(double CurrTime, double ElapsedTime, bool DoUpdateUI) override final;
+    virtual const Char*                GetSampleName() const override final { return "Ming|Texture"; }
+    virtual DesiredApplicationSettings GetDesiredApplicationSettings(bool IsInitialization) override
+    {
+        DesiredApplicationSettings setting{};
+        setting.WindowWidth  = 1280;
+        setting.WindowHeight = 720;
+        setting.Flags        = DesiredApplicationSettings::SETTING_FLAG_WINDOW_WIDTH | DesiredApplicationSettings::SETTING_FLAG_WINDOW_HEIGHT;
 
-    virtual void Render() override final;
-    virtual void Update(double CurrTime, double ElapsedTime, bool DoUpdateUI) override final;
-
-    virtual const Char* GetSampleName() const override final { return "Ming|Texture"; }
+        return setting;
+    }
 
 private:
     void CreatePipelineState();
@@ -44,42 +51,20 @@ SampleBase* CreateSample()
 void Tutorial03_Texturing::CreatePipelineState()
 {
     GraphicsPipelineStateCreateInfo PSOCreateInfo;
-
-    PSOCreateInfo.PSODesc.Name = "Cube PSO";
-
-    // This is a graphics pipeline
-    PSOCreateInfo.PSODesc.PipelineType = PIPELINE_TYPE_GRAPHICS;
-
-    // clang-format off
-    // This tutorial will render to a single render target
+    PSOCreateInfo.PSODesc.Name                                  = "Cube PSO";
+    PSOCreateInfo.PSODesc.PipelineType                          = PIPELINE_TYPE_GRAPHICS;
     PSOCreateInfo.GraphicsPipeline.NumRenderTargets             = 1;
-    // Set render target format which is the format of the swap chain's color buffer
     PSOCreateInfo.GraphicsPipeline.RTVFormats[0]                = m_pSwapChain->GetDesc().ColorBufferFormat;
-    // Set depth buffer format which is the format of the swap chain's back buffer
     PSOCreateInfo.GraphicsPipeline.DSVFormat                    = m_pSwapChain->GetDesc().DepthBufferFormat;
-    // Primitive topology defines what kind of primitives will be rendered by this pipeline state
     PSOCreateInfo.GraphicsPipeline.PrimitiveTopology            = PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
-    // Cull back faces
     PSOCreateInfo.GraphicsPipeline.RasterizerDesc.CullMode      = CULL_MODE_BACK;
-    // Enable depth testing
     PSOCreateInfo.GraphicsPipeline.DepthStencilDesc.DepthEnable = True;
-    // clang-format on
 
     ShaderCreateInfo ShaderCI;
-    // Tell the system that the shader source code is in HLSL.
-    // For OpenGL, the engine will convert this into GLSL under the hood.
-    ShaderCI.SourceLanguage = SHADER_SOURCE_LANGUAGE_HLSL;
-
-    // OpenGL backend requires emulated combined HLSL texture samplers (g_Texture + g_Texture_sampler combination)
+    ShaderCI.SourceLanguage                  = SHADER_SOURCE_LANGUAGE_HLSL;
+    ShaderCI.CompileFlags                    = SHADER_COMPILE_FLAG_PACK_MATRIX_ROW_MAJOR;
     ShaderCI.Desc.UseCombinedTextureSamplers = true;
 
-    // Pack matrices in row-major order
-    ShaderCI.CompileFlags = SHADER_COMPILE_FLAG_PACK_MATRIX_ROW_MAJOR;
-
-    // Presentation engine always expects input in gamma space. Normally, pixel shader output is
-    // converted from linear to gamma space by the GPU. However, some platforms (e.g. Android in GLES mode,
-    // or Emscripten in WebGL mode) do not support gamma-correction. In this case the application
-    // has to do the conversion manually.
     ShaderMacro Macros[] = {{"CONVERT_PS_OUTPUT_TO_GAMMA", m_ConvertPSOutputToGamma ? "1" : "0"}};
     ShaderCI.Macros      = {Macros, _countof(Macros)};
 
@@ -87,7 +72,7 @@ void Tutorial03_Texturing::CreatePipelineState()
     RefCntAutoPtr<IShaderSourceInputStreamFactory> pShaderSourceFactory;
     m_pEngineFactory->CreateDefaultShaderSourceStreamFactory(nullptr, &pShaderSourceFactory);
     ShaderCI.pShaderSourceStreamFactory = pShaderSourceFactory;
-    // Create a vertex shader
+
     RefCntAutoPtr<IShader> pVS;
     {
         ShaderCI.Desc.ShaderType = SHADER_TYPE_VERTEX;
@@ -95,12 +80,9 @@ void Tutorial03_Texturing::CreatePipelineState()
         ShaderCI.Desc.Name       = "Cube VS";
         ShaderCI.FilePath        = "02_texture_cube.vsh";
         m_pDevice->CreateShader(ShaderCI, &pVS);
-        // Create dynamic uniform buffer that will store our transformation matrix
-        // Dynamic buffers can be frequently updated by the CPU
-        CreateUniformBuffer(m_pDevice, sizeof(float4x4), "VS constants CB", &m_VSConstants);
     }
+    CreateUniformBuffer(m_pDevice, sizeof(float4x4), "VS constants CB", &m_VSConstants);
 
-    // Create a pixel shader
     RefCntAutoPtr<IShader> pPS;
     {
         ShaderCI.Desc.ShaderType = SHADER_TYPE_PIXEL;
@@ -110,48 +92,29 @@ void Tutorial03_Texturing::CreatePipelineState()
         m_pDevice->CreateShader(ShaderCI, &pPS);
     }
 
-    // clang-format off
-    // Define vertex shader input layout
     LayoutElement LayoutElems[] =
-    {
-        // Attribute 0 - vertex position
-        LayoutElement{0, 0, 3, VT_FLOAT32, False},
-        // Attribute 1 - texture coordinates
-        LayoutElement{1, 0, 2, VT_FLOAT32, False}
-    };
-    // clang-format on
+        {
+            LayoutElement{0, 0, 3, VT_FLOAT32, False}, // position
+            LayoutElement{1, 0, 2, VT_FLOAT32, False}  // uv
+        };
 
     PSOCreateInfo.pVS = pVS;
     PSOCreateInfo.pPS = pPS;
 
     PSOCreateInfo.GraphicsPipeline.InputLayout.LayoutElements = LayoutElems;
     PSOCreateInfo.GraphicsPipeline.InputLayout.NumElements    = _countof(LayoutElems);
+    PSOCreateInfo.PSODesc.ResourceLayout.DefaultVariableType  = SHADER_RESOURCE_VARIABLE_TYPE_STATIC;
 
-    // Define variable type that will be used by default
-    PSOCreateInfo.PSODesc.ResourceLayout.DefaultVariableType = SHADER_RESOURCE_VARIABLE_TYPE_STATIC;
-
-    // clang-format off
     // Shader variables should typically be mutable, which means they are expected
     // to change on a per-instance basis
-    ShaderResourceVariableDesc Vars[] = 
-    {
-        {SHADER_TYPE_PIXEL, "g_Texture", SHADER_RESOURCE_VARIABLE_TYPE_MUTABLE}
-    };
-    // clang-format on
+    ShaderResourceVariableDesc Vars[] = {{SHADER_TYPE_PIXEL, "g_Texture", SHADER_RESOURCE_VARIABLE_TYPE_MUTABLE}};
+
     PSOCreateInfo.PSODesc.ResourceLayout.Variables    = Vars;
     PSOCreateInfo.PSODesc.ResourceLayout.NumVariables = _countof(Vars);
 
-    // clang-format off
-    // Define immutable sampler for g_Texture. Immutable samplers should be used whenever possible
-    SamplerDesc SamLinearClampDesc
-    {
-        FILTER_TYPE_LINEAR, FILTER_TYPE_LINEAR, FILTER_TYPE_LINEAR, 
-        TEXTURE_ADDRESS_CLAMP, TEXTURE_ADDRESS_CLAMP, TEXTURE_ADDRESS_CLAMP
-    };
-    ImmutableSamplerDesc ImtblSamplers[] = 
-    {
-        {SHADER_TYPE_PIXEL, "g_Texture", SamLinearClampDesc}
-    };
+    SamplerDesc          SamLinearClampDesc{FILTER_TYPE_LINEAR, FILTER_TYPE_LINEAR, FILTER_TYPE_LINEAR, TEXTURE_ADDRESS_CLAMP, TEXTURE_ADDRESS_CLAMP, TEXTURE_ADDRESS_CLAMP};
+    ImmutableSamplerDesc ImtblSamplers[] = {{SHADER_TYPE_PIXEL, "g_Texture", SamLinearClampDesc}};
+
     // clang-format on
     PSOCreateInfo.PSODesc.ResourceLayout.ImmutableSamplers    = ImtblSamplers;
     PSOCreateInfo.PSODesc.ResourceLayout.NumImmutableSamplers = _countof(ImtblSamplers);
@@ -269,7 +232,6 @@ void Tutorial03_Texturing::Initialize(const SampleInitInfo& InitInfo)
     LoadTexture();
 }
 
-// Render a frame
 void Tutorial03_Texturing::Render()
 {
     ITextureView* pRTV = m_pSwapChain->GetCurrentBackBufferRTV();
@@ -314,20 +276,11 @@ void Tutorial03_Texturing::Update(double CurrTime, double ElapsedTime, bool DoUp
 {
     SampleBase::Update(CurrTime, ElapsedTime, DoUpdateUI);
 
-    // Apply rotation
     float4x4 CubeModelTransform = float4x4::RotationY(static_cast<float>(CurrTime) * 1.0f) * float4x4::RotationX(-PI_F * 0.1f);
-
-    // Camera is at (0, 0, -5) looking along the Z axis
-    float4x4 View = float4x4::Translation(0.f, 0.0f, 5.0f);
-
-    // Get pretransform matrix that rotates the scene according the surface orientation
-    float4x4 SrfPreTransform = GetSurfacePretransformMatrix(float3{0, 0, 1});
-
-    // Get projection matrix adjusted to the current screen orientation
-    float4x4 Proj = GetAdjustedProjectionMatrix(PI_F / 4.0f, 0.1f, 100.f);
-
-    // Compute world-view-projection matrix
-    m_WorldViewProjMatrix = CubeModelTransform * View * SrfPreTransform * Proj;
+    float4x4 View               = float4x4::Translation(0.f, 0.0f, 5.0f); // Camera is at (0, 0, -5)
+    float4x4 SrfPreTransform    = GetSurfacePretransformMatrix(float3{0, 0, 1});
+    float4x4 Proj               = GetAdjustedProjectionMatrix(PI_F / 4.0f, 0.1f, 100.f);
+    m_WorldViewProjMatrix       = CubeModelTransform * View * SrfPreTransform * Proj;
 }
 
 } // namespace Diligent
